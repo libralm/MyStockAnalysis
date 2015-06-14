c2a00520a33119df4563d05da913fec66ee8459d
libralm <63077217@qq.com>
2015-06-14 23:33:05 +0800

    添加了ossservice的基本配置。

--- src/com/libra/stockanalysisi/engine/impl/AliyunOSSServiceImpl.java
+++ src/com/libra/stockanalysisi/engine/impl/AliyunOSSServiceImpl.java
@@ -6,6 +6,9 @@ import android.content.Context;
 
 import com.alibaba.sdk.android.oss.OSSService;
 import com.alibaba.sdk.android.oss.OSSServiceProvider;
+import com.alibaba.sdk.android.oss.model.AuthenticationType;
+import com.alibaba.sdk.android.oss.model.ClientConfiguration;
+import com.alibaba.sdk.android.oss.model.TokenGenerator;
 import com.libra.stockanalysisi.engine.IOSSService;
 
 public class AliyunOSSServiceImpl implements IOSSService {
@@ -13,8 +16,31 @@ public class AliyunOSSServiceImpl implements IOSSService {
 	private OSSService m_OSSService;
 	
 	public AliyunOSSServiceImpl(Context pContext){
-		m_OSSService = OSSServiceProvider.getService();
-		m_OSSService.setApplicationContext(pContext);
+		initOSSSevice(pContext);
+	}
+
+	private void initOSSSevice(Context pContext) {
+		// TODO Auto-generated method stub
+		OSSService ossService = OSSServiceProvider.getService();
+		ossService.setApplicationContext(pContext);
+		ossService.setGlobalDefaultHostId("oss-cn-beijing.aliyuncs.com");//北京节点
+		ossService.setAuthenticationType(AuthenticationType.ORIGIN_AKSK);
+		ossService.setGlobalDefaultTokenGenerator(new TokenGenerator() {
+
+			@Override
+			public String generateToken(String arg0, String arg1, String arg2,
+					String arg3, String arg4, String arg5) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		});
+		ossService.setCustomStandardTimeWithEpochSec(System.currentTimeMillis());
+		ClientConfiguration conf = new ClientConfiguration();
+		conf.setConnectTimeout(15 * 1000); // 设置建连超时时间，默认为30s
+		conf.setSocketTimeout(15 * 1000); // 设置socket超时时间，默认为30s
+		conf.setMaxConnections(50); // 设置全局最大并发连接数，默认为50个
+		ossService.setClientConfiguration(conf);
+		m_OSSService = ossService;
 	}
 
 	@Override
