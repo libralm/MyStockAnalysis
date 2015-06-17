0216e496a3607e115ad31fc554a69a2bdc900698
libralm <63077217@qq.com>
2015-06-17 15:26:58 +0800

    新增北京时间服务接口

---
+++ src/com/libra/stockanalysisi/engine/impl/BeijingTimeSeviceImpl.java
@@ -0,0 +1,34 @@
+package com.libra.stockanalysisi.engine.impl;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Date;
+import java.util.TimeZone;
+
+import com.libra.stockanalysisi.engine.ITimeService;
+
+public class BeijingTimeSeviceImpl implements ITimeService {
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public boolean isDealTime() throws IOException {
+		// TODO Auto-generated method stub
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+8")); // 时区设置  
+	       URL url=new URL("http://www.bjtime.cn");//取得资源对象  
+	       URLConnection uc=url.openConnection();//生成连接对象  
+	       uc.connect(); //发出连接  
+	       long ld=uc.getDate(); //取得网站日期时间（时间戳）  
+	       Date date=new Date(ld); //转换为标准时间对象  
+	       //分别取得时间中的小时，分钟和秒，并输出  
+	       int year = date.getYear();
+	       int month = date.getMonth();
+	       int day = date.getDate();
+	       Date dealTime = new Date(year, month, day, 8, 24);
+	       if(date.after(dealTime)){
+	    	   return true;
+	       }
+		return false;
+	}
+
+}
