a60b2340b1759388311a844ed0cfe2d63b8f81c6
libralm <63077217@qq.com>
2015-06-19 17:46:25 +0800

    保存数据失败异常

---
+++ src/com/libra/stockanalysisi/throwable/SaveDataException.java
@@ -0,0 +1,17 @@
+package com.libra.stockanalysisi.throwable;
+
+import java.io.IOException;
+
+public class SaveDataException extends IOException {
+
+	public SaveDataException(String pMsg) {
+		// TODO Auto-generated constructor stub
+		super(pMsg);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 795508848443146329L;
+
+}
