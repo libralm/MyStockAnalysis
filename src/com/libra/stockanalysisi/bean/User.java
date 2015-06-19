9ffa908f0f3f4d73c05913a33a80976ebc2d7dfe
libralm <63077217@qq.com>
2015-06-19 17:47:56 +0800

    用户类

---
+++ src/com/libra/stockanalysisi/bean/User.java
@@ -0,0 +1,12 @@
+package com.libra.stockanalysisi.bean;
+
+import cn.bmob.v3.BmobUser;
+
+public class User extends BmobUser{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7864376486540843043L;
+	
+}
