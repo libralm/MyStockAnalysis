e1f311eab151c01036fec9bbb1fb96fc2960c3e0
libralm <63077217@qq.com>
2015-06-20 12:10:24 +0800

    完成门面实现

--- src/com/libra/stockanalysisi/engine/impl/UserBussinessFacde.java
+++ src/com/libra/stockanalysisi/engine/impl/UserBussinessFacde.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import com.libra.stockanalysisi.bean.User;
 import com.libra.stockanalysisi.engine.FacdeService;
 import com.libra.stockanalysisi.engine.IUserManagerService;
-import com.libra.stockanalysisi.engine.UserManagerCallback;
+import com.libra.stockanalysisi.engine.NetDataCallback;
 
 public class UserBussinessFacde implements IUserManagerService, FacdeService{
 	
@@ -17,14 +17,14 @@ public class UserBussinessFacde implements IUserManagerService, FacdeService{
 
 	@Override
 	public void regiester(String pUserName, String pPassword, String email,
-			UserManagerCallback pCallback) {
+			NetDataCallback pCallback) {
 		// TODO Auto-generated method stub
 		m_UserManagerService.regiester(pUserName, pPassword, email, pCallback);
 	}
 
 	@Override
 	public void loginBySmsCode(int pPhoneNum, String pSmscode,
-			UserManagerCallback pCallback) {
+			NetDataCallback pCallback) {
 		// TODO Auto-generated method stub
 		m_UserManagerService.loginBySmsCode(pPhoneNum, pSmscode, pCallback);
 	}
@@ -36,65 +36,65 @@ public class UserBussinessFacde implements IUserManagerService, FacdeService{
 	}
 
 	@Override
-	public void logout(UserManagerCallback pCallback) {
+	public void logout(NetDataCallback pCallback) {
 		// TODO Auto-generated method stub
 		m_UserManagerService.logout(pCallback);
 	}
 
 	@Override
-	public void updateUserInfo(User pUser, UserManagerCallback pCallback) {
+	public void updateUserInfo(User pUser, NetDataCallback pCallback) {
 		// TODO Auto-generated method stub
 		m_UserManagerService.updateUserInfo(pUser, pCallback);
 	}
 
 	@Override
-	public void queryUser(User pUser, UserManagerCallback pCallback) {
+	public void queryUser(User pUser, NetDataCallback pCallback) {
 		// TODO Auto-generated method stub
 		m_UserManagerService.queryUser(pUser, pCallback);
 	}
 
 	@Override
 	public void resetPasswordByEmail(String pEmail,
-			UserManagerCallback pCallback) {
+			NetDataCallback pCallback) {
 		// TODO Auto-generated method stub
 		m_UserManagerService.resetPasswordByEmail(pEmail, pCallback);
 	}
 
 	@Override
-	public void requestEmailVerify(String pEmail, UserManagerCallback pCallback) {
+	public void requestEmailVerify(String pEmail, NetDataCallback pCallback) {
 		// TODO Auto-generated method stub
 		m_UserManagerService.requestEmailVerify(pEmail, pCallback);
 	}
 
 	@Override
-	public void requestSMSCode(int pPhoneNum, UserManagerCallback pCallback) {
+	public void requestSMSCode(int pPhoneNum, NetDataCallback pCallback) {
 		// TODO Auto-generated method stub
 		m_UserManagerService.requestSMSCode(pPhoneNum, pCallback);
 	}
 
 	@Override
 	public void verifySmsCode(int pPhoneNum, String pSMSCode,
-			UserManagerCallback pCallback) {
+			NetDataCallback pCallback) {
 		// TODO Auto-generated method stub
 		m_UserManagerService.verifySmsCode(pPhoneNum, pSMSCode, pCallback);
 	}
 
 	@Override
-	public void resetPasswordByPhone(int phoneNum, UserManagerCallback pCallback) {
+	public void resetPasswordByPhone(int phoneNum, NetDataCallback pCallback) {
 		// TODO Auto-generated method stub
 		m_UserManagerService.resetPasswordByPhone(phoneNum, pCallback);
 	}
 
 	@Override
 	public void bindingPhoneNum(int pPhoneNum, String pSmsCode,
-			UserManagerCallback pCallback) {
+			NetDataCallback pCallback) {
 		// TODO Auto-generated method stub
 		m_UserManagerService.bindingPhoneNum(pPhoneNum, pSmsCode, pCallback);
 	}
 
 	@Override
 	public void login(String pAccount, String pPassword,
-			UserManagerCallback pCallback) {
+			NetDataCallback pCallback) {
 		// TODO Auto-generated method stub
 		m_UserManagerService.login(pAccount, pPassword, pCallback);
 	}
