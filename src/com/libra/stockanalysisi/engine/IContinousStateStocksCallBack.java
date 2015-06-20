574f2468a1c88bb23b327cb4d598587a636b9c20
libralm <63077217@qq.com>
2015-06-20 12:03:00 +0800

    添加了接口注释

--- src/com/libra/stockanalysisi/engine/IContinousStateStocksCallBack.java
+++ src/com/libra/stockanalysisi/engine/IContinousStateStocksCallBack.java
@@ -2,6 +2,11 @@ package com.libra.stockanalysisi.engine;
 
 import com.libra.stockanalysisi.bean.Stock;
 
+/**
+ * 持续状态计算后的回调。
+ * @author liaomin
+ *
+ */
 public interface IContinousStateStocksCallBack {
 
 	void continusFallingStocks(Stock[] result);
