19bc4664751d76ccc52dfa66ea02dfa323638eb4
libralm <63077217@qq.com>
2015-06-23 17:45:18 +0800

    添加的存储路径

--- src/com/libra/stockanalysisi/engine/impl/BmobDataSyncServiceImpl.java
+++ src/com/libra/stockanalysisi/engine/impl/BmobDataSyncServiceImpl.java
@@ -1,18 +1,24 @@
 package com.libra.stockanalysisi.engine.impl;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import android.content.Context;
+import android.os.Environment;
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.listener.FindListener;
 import cn.bmob.v3.listener.SaveListener;
 
+import com.bmob.BmobConfiguration;
+import com.bmob.BmobPro;
 import com.bmob.BmobProFile;
 import com.bmob.btp.callback.DownloadListener;
 import com.bmob.btp.callback.UploadListener;
 import com.libra.stockanalysisi.bean.NetFileData;
 import com.libra.stockanalysisi.engine.IDataSyncService;
+import com.libra.stockanalysisi.engine.IPersistenceService;
 
 public class BmobDataSyncServiceImpl implements IDataSyncService {
 	
@@ -20,6 +26,10 @@ public class BmobDataSyncServiceImpl implements IDataSyncService {
 	
 	public BmobDataSyncServiceImpl(Context pContext){
 		m_Context = pContext;
+		File file = new File(Environment.getExternalStorageDirectory()
+				.getAbsolutePath() + "/" + IPersistenceService.M_DIRNAME);
+		BmobConfiguration config = new BmobConfiguration.Builder(pContext).customExternalCacheDir(file.getAbsolutePath()).build();
+		BmobPro.getInstance(pContext).initConfig(config);
 	}
 	
 
@@ -74,10 +84,15 @@ public class BmobDataSyncServiceImpl implements IDataSyncService {
 	}
 
 	@Override
-	public void requestAllNetFiles(String pUrl, final AllNetFilesCallback pCallback) {
+	public	void requestNetFiles(Date pBeginDate, Date pEndDate, final AllNetFilesCallback pCallback){
 		// TODO Auto-generated method stub
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String strBeginDae = sdf.format(pBeginDate);
+		String strEndDate = sdf.format(pEndDate);
 		BmobQuery<NetFileData> query = new BmobQuery<NetFileData>("NetFileData");
 		query.setLimit(1000);
+		query.addWhereGreaterThan("m_FileName", strBeginDae);
+		query.addWhereLessThan("m_FileName", strEndDate);
 		query.findObjects(m_Context, new FindListener<NetFileData>() {
 			
 			@Override
