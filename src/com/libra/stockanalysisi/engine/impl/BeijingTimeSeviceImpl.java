f7edacfd875e80e43dd8ebd51d2f301dd026ace8
libralm <63077217@qq.com>
2015-07-10 17:32:59 +0800

    因为www.bjtime.cn获取的时间不对，所以改成了百度的了。

--- src/com/libra/stockanalysisi/engine/impl/BeijingTimeSeviceImpl.java
+++ src/com/libra/stockanalysisi/engine/impl/BeijingTimeSeviceImpl.java
@@ -15,7 +15,7 @@ class BeijingTimeSeviceImpl implements ITimeService {
 	public boolean isDealTime() throws IOException {
 		// TODO Auto-generated method stub
 		TimeZone.setDefault(TimeZone.getTimeZone("GMT+8")); // 时区设置  
-	       URL url=new URL("http://www.bjtime.cn");//取得资源对象  
+	       URL url=new URL("http://www.baidu.com");//取得资源对象  
 	       URLConnection uc=url.openConnection();//生成连接对象  
 	       uc.connect(); //发出连接  
 	       long ld=uc.getDate(); //取得网站日期时间（时间戳）  
