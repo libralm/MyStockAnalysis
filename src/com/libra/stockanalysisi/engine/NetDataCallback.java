a758f443aee7d0d887e0dd0eb37b53d127f3f96e
libralm <63077217@qq.com>
2015-07-22 17:21:29 +0800

    新增无需网络请求的接口

--- src/com/libra/stockanalysisi/engine/NetDataCallback.java
+++ src/com/libra/stockanalysisi/engine/NetDataCallback.java
@@ -2,7 +2,20 @@ package com.libra.stockanalysisi.engine;
 
 public interface NetDataCallback{
 	
+	/**
+	 * 数据成功
+	 */
 	void onSuccess();
 	
+	/**
+	 * 失败
+	 * @param pCode
+	 * @param pMsg
+	 */
 	void onFailure(int pCode, String pMsg);
+	
+	/**
+	 * 无需请求网络
+	 */
+	void onSkip();
 }
